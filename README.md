49ec9bac98872a586424f76b178a50004c4aab3b
root <root@BCPR003.cs1local>
2016-06-17 19:59:53 +0000

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Odoo-Multi-Theme
+
+https://shapebootstrap.net/item/1524960-multi-free-responsive-onepage-html-template/
---
+++ multi_theme/__openerp__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Multi Theme",
+
+    'summary': """
+    Custom theme""",
+
+    'description': """
+    Odoo version free Multi HTML Template https://shapebootstrap.net/item/1524960-multi-free-responsive-onepage-html-template/""",
+
+    'author': "Mario Gielissen",
+    'website': "http://www.openworx.nl",
+
+    'category': 'Theme',
+    'version': '0.1',
+
+    'depends': [
+        'base',
+        'website'
+    ],
+    'data': [
+        'views/layout.xml',
+        'views/assets.xml'
+    ],
+
+    'demo': [
+    ],
+
+    'tests': [
+    ],
+}
